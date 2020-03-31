f39f8b0d49bc8b1087cb17407aa267fe16672968
Janus <ysangkok@gmail.com>
2020-03-31 22:27:56 +0000

    initial commit

---
+++ src/ClassLibrary1/App.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary1
+{
+    public class App: Xamarin.Forms.Application
+    {
+        public App() {
+            this.MainPage = new ReceivePage();
+        }
+    }
+}
---
+++ src/ClassLibrary1/ReceivePage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ZXing;
+using ZXing.Net.Mobile.Forms;
+using ZXing.Common;
+
+namespace ClassLibrary1
+{
+    public partial class ReceivePage : ContentPage
+    {
+        public ReceivePage()
+        {
+            Extensions.LoadFromXaml(this, typeof(ReceivePage));
+        }
+
+        override protected void OnAppearing()
+        {
+            var mainLayout = (StackLayout) base.FindByName("mainLayout");
+            var size = 200;
+            var encodingOptions = new EncodingOptions();
+            encodingOptions.Height = size;
+            encodingOptions.Width = size;
+            var barCode = new ZXingBarcodeImageView();
+            barCode.HorizontalOptions = LayoutOptions.Center;
+            barCode.VerticalOptions = LayoutOptions.Center;
+            barCode.BarcodeFormat = BarcodeFormat.QR_CODE;
+            barCode.BarcodeValue = "abc";
+            barCode.HeightRequest = (float)size;
+            barCode.WidthRequest = (float)size;
+            barCode.BarcodeOptions = encodingOptions;
+            mainLayout.Children.Add(barCode);
+        }
+    }
+}
